704e1d54573853acf46aa5e2a959e9c475b05f37
yashikkhanna <140069199+yashikkhanna@users.noreply.github.com>
2026-03-18 11:22:49 +0530

    Fixed Prescription creation and fetching

--- backend/router/prescriptionRouter.js
+++ backend/router/prescriptionRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { createPrescription , getPrescriptionsToRemind , getPrescriptionByAppointment,updatePrescription} from "../controller/prescriptionController.js";
+import { createPrescription , getPrescriptionsByPatient, getPrescriptionsToRemind ,updatePrescription} from "../controller/prescriptionController.js";
 import { isDoctorAuthenticated, isPatientAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/create-Prescription", isPatientAuthenticated, createPrescription);
 router.get("/reminders", getPrescriptionsToRemind);
-router.get("/prescriptions/:appointmentId", isPatientAuthenticated, getPrescriptionByAppointment);
+router.get("/prescriptions/:patientId", isPatientAuthenticated,getPrescriptionsByPatient );
 router.put("/updatePrescriptions/:id", isPatientAuthenticated, updatePrescription);
 
 export default router;
